59ff160665177570cf00d5cdd0549e524637e9d6
nadwaso2000 <nadwaso2000@gmail.com>
2025-04-10 19:05:11 +0300

    update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,18 @@
 export default function Card(props)
 {
     return(
-        <div className="lg:w-[40%] md:[50%] w-[234px] flex justify-center items-center flex-col gap-4 p-4 bg-[#b5ebe5] rounded-[20px] shadow-xl">
-             <div className="w-full h-[40px] flex justify-start items-center pl-4 gap-3">
+        <div className="lg:w-[40%] md:[50%] sm:w-[40%] w-[95%] flex justify-center items-center flex-col gap-4 p-3 md:p-4 bg-[#dee3f3] rounded-[20px] shadow-xl">
+             <div className="w-full lg:h-[40px] flex lg:flex-row flex-col justify-start lg:items-center items-start pl-0 lg:pl-4 gap-3">
                 <img className="w-[50px] h-[50px] rounded-full bg-gray-300" src={props.profile_image} alt=""/>
-                <p className="font-bold text-[18px] text-[#000000]">{props.userName}</p>
+                <p className="font-bold text-[13px] md:text-[18px] text-[#000000]">{props.userName}</p>
              </div>
-             <div className="w-full h-[190px] lg:h-[300px] flex justify-center items-center">
+             <div className="w-full h-[220px] md:h-[300px] lg:h-[350px] flex justify-center items-center">
                 <img className="w-full h-full bg-slate-600" src={props.image} alt="not found"/>
              </div>
              <div className="flex flex-col justify-center items-start gap-2 w-[97%] border-b-2 border-b-[#c4c1c1]">
                 <p className="font-normal text-gray-500 text-[16px]">{props.created_at}</p>
-                <h2 className="font-normal text-[#000000] text-[24px]">{props.title}</h2>
-                <p className="font-medium text-[#000000] text-[16px]">{props.body}</p>
+                <h2 className="font-normal text-[#000000] text-[17px] sm:text-[20px] md:text-[24px]">{props.title}</h2>
+                <p className="font-medium text-[#000000] w-[full] text-[13px] md:text-[16px]">{props.body}</p>
              </div>
              <div className="w-full h-[40px] flex justify-start items-center pl-4">
                 <p>Comments {props.comments_count}</p>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,20 @@
 //import {Modal , Button , Form} from 'react-bootstrap'
-import {useContext, useState} from 'react'
+import {useState} from 'react'
 import ModalComponent from './Modal';
 import Cookie from 'cookie-universal'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+
 
 
 export default function Navbar()
 {
   const[show , setShow] = useState(false);
-  const[state , setState] = useState(null)
+  const[state , setState] = useState(null);
+  const[open , setOpen] = useState(true);
 
   const cookie = Cookie()
   const getCookie = cookie.get('media')
-  const getUsername = cookie.get('username')
-  const getPassword = cookie.get('password')
-  //const nav = useNavigate()
 
   
 
@@ -31,26 +30,43 @@ export default function Navbar()
 
   const handleLogout = () =>
      {
-       // try{
-          // let res = axios.post("https://tarmeezAcademy.com/api/v1/logout" , 
-          //  {
-             //username :  getUsername ,
-             //password :  getPassword
-              
-           // } ,
-          // { headers : {
-               // Authorization : "Bearer " + getCookie ,
-               // Accept : "application/json"
-             // }
-          // }
-          // )
-       // }
-       // catch(err){
           cookie.remove('media')
           window.location.pathname = '/'
-         // console.log(err)
-          
-       // }
+     }
+
+     const handleOpenMenu = () => 
+     {
+       setOpen(false)
+       const ul = document.getElementById("ulEle");
+       const btn = document.getElementById("btn");
+       const divE = document.getElementById("divEle");
+       console.log(divE)
+
+       ul.classList.remove("hidden")
+       btn && btn.classList.remove("hidden")
+       divE && divE.classList.remove("hidden")
+
+       ul.classList.add("flex")
+       btn && btn.classList.add("flex")
+       divE && divE.classList.add("flex")
+       
+     }
+
+     const handleCloseMenu = () => 
+     {
+        setOpen(true)
+        const ul = document.getElementById("ulEle");
+        const btn = document.getElementById("btn");
+        const divE = document.getElementById("divEle");
+ 
+        ul.classList.add("hidden")
+        btn && btn.classList.add("hidden")
+        divE && divE.classList.add("hidden")
+ 
+        ul.classList.remove("flex")
+        btn && btn.classList.remove("flex")
+        divE && divE.classList.remove("flex")
+        
      }
     return(
       <>
@@ -67,19 +83,25 @@ export default function Navbar()
             ButtonName="Login"
          />
         }
-        <div className="w-[80%] h-[80px] bg-[#e7e1e1] flex justify-between items-center px-6 shadow-2xl rounded-[10px]">
-          <ul className="flex justify-center items-center gap-4 list-none px-2 mt-[10px]">
-            <li className="font-bold text-[21px] text-[#000000]">My Platform</li>
-            <li className="font-normal text-[19px] text-[#3f3f3f]">Home</li>
-            <li className="font-semibold text-[19px] text-[#aaa9a9]">Profile</li>
+        <div className=" w-[95%] sm:w-[80%] md:h-[80px] md:py-0 py-2 bg-[#e7e1e1] flex md:flex-row flex-col md:justify-between justify-center md:items-center items-end px-6 shadow-2xl rounded-[10px]">
+          { open ?
+            <MdMenu className='md:hidden flex w-[30px]  h-[30px] text-gray-500 fit-content' onClick={handleOpenMenu} />
+             :
+            <MdClose className='md:hidden flex w-[30px]  h-[30px] text-gray-500 fit-content' onClick={handleCloseMenu}/>
+          }
+        
+          <ul className="md:flex md:flex-row flex-col hidden  justify-center items-end md:items-center gap-4 list-none px-2 mt-[10px]" id='ulEle'>
+            <li className="font-bold text-[16px] md:text-[18px] lg:text-[21px] text-[#000000]">My Platform</li>
+            <li className="font-normal md:text-[16px] lg:text-[19px] text-[#949393]">Home</li>
+            <li className="font-semibold md:text-[16px] lg:text-[19px] text-[#aaa9a9]">Profile</li>
           </ul>
           {
             getCookie ?
-            <button onClick={handleLogout} className='flex justify-center items-center bg-red-500 text-gray-300 text-[17px] w-[70px] p-2 rounded-[10px] hover:border-solid hover:border-red-500 hover:border-[2px] hover:bg-white hover:text-red-500 font-semibold'>Logout</button>
+            <button onClick={handleLogout} className='md:flex hidden justify-center items-center bg-red-500 text-gray-300 text-[15px] md:text-[17px] w-[70px] p-2 rounded-[10px] hover:border-solid hover:border-red-500 hover:border-[2px] hover:bg-white hover:text-red-500 font-semibold' id='btn'>Logout</button>
             :
-          <div className="flex justify-center items-center gap-4">
-              <button onClick={handleShowLogin} className="flex justify-center items-center bg-green-500 text-gray-300 text-[17px] w-[70px] p-2 rounded-[10px] hover:border-solid hover:border-green-500 hover:border-[2px] hover:bg-white hover:text-green-500 font-semibold">Login</button>
-              <button onClick={handleShowRegister} className="flex justify-center items-center bg-green-500 text-gray-300 text-[17px] w-[90px] p-2 rounded-[10px]  hover:border-solid hover:border-green-500 hover:border-[2px] hover:bg-white hover:text-green-500 font-semibold">Register</button>
+          <div className="md:flex md:flex-row flex-col hidden justify-center md:items-center items-end gap-4" id='divEle'>
+              <button onClick={handleShowLogin} className="flex justify-center items-center bg-green-500 text-gray-300 text-[15px] md:text-[17px] w-[70px] p-2 rounded-[10px] hover:border-solid hover:border-green-500 hover:border-[2px] hover:bg-white hover:text-green-500 font-semibold">Login</button>
+              <button onClick={handleShowRegister} className="flex justify-center items-center bg-green-500 text-gray-300  text-[15px] md:text-[17px] w-[90px] p-2 rounded-[10px]  hover:border-solid hover:border-green-500 hover:border-[2px] hover:bg-white hover:text-green-500 font-semibold">Register</button>
           </div>
           }
         </div>
